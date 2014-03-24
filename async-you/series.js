5f68e11e9feb429cab4218c35a59fed485041bca
edgarjcfn <edgar.neto@wooga.com>
2014-03-24 22:28:13 +0100

    Improved series.js

--- async-you/series.js
+++ async-you/series.js
@@ -2,12 +2,8 @@ var async = require('async');
 var http = require('http');
 
 async.series({
-  requestOne: function (callback) {
-    httpGet(process.argv[2], callback);
-  } ,
-  requestTwo: function(callback) {
-    httpGet(process.argv[3], callback);
-  }
+  requestOne:  httpGet(process.argv[2]),
+  requestTwo:  httpGet(process.argv[3])
  },
  function (err, result) {
    console.log(result);
@@ -15,19 +11,21 @@ async.series({
 );
 
 
-function httpGet(url, callback) {
-    http.get(url, function (response) {
-     var contents = '';
-     response.on('data', function(chunk) {
-        contents += chunk
-     });
+function httpGet(url) {
+  return function(callback) {
+      http.get(url, function (response) {
+       var contents = '';
+       response.on('data', function(chunk) {
+          contents += chunk
+       });
 
-     response.on('end', function() {
-        callback(null, contents);
-     });
+       response.on('end', function() {
+          callback(null, contents);
+       });
 
-     response.on('error', function(e) {
-         callback(e);
-     });
-  });
+       response.on('error', function(e) {
+           callback(e);
+       });
+    });
+  };
 }
